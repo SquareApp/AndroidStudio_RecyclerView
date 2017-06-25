85424900341127c897285af045a4b36d4716e5b0
SquareApp <squareapp.games@gmail.com>
2017-06-25 21:58:53 +0200

    Update CustomAdapter.java

--- android_training/CustomAdapter.java
+++ android_training/CustomAdapter.java
@@ -1,11 +1,16 @@
 package com.squareapp.android_training;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -14,6 +19,8 @@ import java.util.ArrayList;
  */
 
 public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
 
 
    private ArrayList<MyObject> dataList;
@@ -47,29 +54,33 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position)
     {
+
+        int currentPosition = position -1;
+
         if(getItemViewType(position) == 1)
         {
 
         }
         else
         {
-            ((NormaleViewHolder)holder).taskText.setText(dataList.get(position-1).getTask());
-            ((NormaleViewHolder)holder).descriptionText.setText(dataList.get(position-1).getDescription());
-            ((NormaleViewHolder)holder).dateText.setText(dataList.get(position-1).getDate());
+            ((NormaleViewHolder)holder).taskText.setText(dataList.get(currentPosition).getTask());
+            ((NormaleViewHolder)holder).descriptionText.setText(dataList.get(currentPosition).getDescription());
            
-           
-           
-           //here you set up your onCLickListeners and all that stuff
-           
-           
-           
+           //Picasso loads images from web and places them into an imageview
+            Picasso.with(context)
+                    .load(dataList.get(currentPosition).getImageUrl())
+                    .placeholder(R.drawable.ic_search_black_36dp)
+                    .resize(1000,800).centerCrop()
+                    .into(((NormaleViewHolder) holder).img);
+
+
+
+
+
+
         }
-       
-       
-       
-       
     }
 
     @Override
@@ -92,18 +103,21 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
         TextView taskText;
         TextView descriptionText;
-        TextView dateText;
+        ImageView img;
+        CardView cardView;
 
 
         public NormaleViewHolder(View itemView)
+
         {
             super(itemView);
 
 
             taskText = (TextView)itemView.findViewById(R.id.taskText);
             descriptionText = (TextView)itemView.findViewById(R.id.descriptionText);
-            dateText = (TextView)itemView.findViewById(R.id.dateText);
-
+            img = (ImageView)itemView.findViewById(R.id.urlImage);
+            cardView = (CardView)itemView.findViewById(R.id.cardView);
+            Log.d("ViewHolder", "load image from url");
         }
     }
 
